adf3b6fffbcd69f8436d3005317a60f968811e60
EricStrohmaier <eric.strohmaier00@gmail.com>
2024-01-31 09:19:12 +0100

    start

---
+++ app/components/MainComponent.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { PageQuery } from "@/tina/__generated__/types";
+import { useTina } from "tinacms/dist/react";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
+
+export default function MainComponent(props: { query: string; variables: { relativePath:string}; data: PageQuery; }) {
+  // data passes though in production mode and data is updated to the sidebar data in edit-mode
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+  });
+
+  const content = data.page.body;
+  const title = data.page.title;
+  return (
+    <>
+      <div >
+        <h1>{title}</h1>
+        <TinaMarkdown content={content} />
+      </div>
+    </>
+  );
+}
+
+
