f8d7066829341a7ea823224c4c5bfbcc00331411
EricStrohmaier <eric.strohmaier00@gmail.com>
2024-02-01 08:32:56 +0100

    cleanup

--- components/app/LayoutPage.tsx
+++ components/app/LayoutPage.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 
 const LayoutPage: FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="p-9 h-full min-h-screen w-full flex items-center justify-center bg-[var(--background)] text-[var(--text)]">
+    <div className=" py-2 sm:p-9 h-full min-h-screen w-full flex items-center justify-center bg-[var(--background)] text-[var(--text)]">
       <div className="flex items-center justify-center min-h-[90%] h-full w-full">
         <div className="flex mx-3 my-6 max-w-7xl h-full w-full rounded-[40px] overflow-hidden bg-[var(--primary)] border-[var(--text)]">
           <div className="border-r-[1px] lg:w-1/3 bg-[var(--primary)]">
